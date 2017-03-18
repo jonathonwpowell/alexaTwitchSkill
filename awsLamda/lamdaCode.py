c10f5071a23219ccc2ec333e81c8ffc526af19e6
jonathonwpowell <jonathonwpowell@gmail.com>
2017-03-18 12:48:51 -0400

    more work on base functionality

--- awsLamda/lamdaCode.py
+++ awsLamda/lamdaCode.py
@@ -28,18 +28,55 @@ def build_response(session_attributes, speechlet_response):
         'sessionAttributes': session_attributes,
         'response': speechlet_response
     }
+#---------------------- Twitch Auth Helpers--------------------------------------
+def get_twitch_auth(session):
+    raise NotImplementedError
+    #TODO
+
+#------------------------Twitch API Helpers -------------------------------------
+def twitch_top_streamers(num_of_response, auth_info):
+    raise NotImplementedError
+
+def twitch_my_top_streamers(auth_info):
+    raise NotImplementedError
+
+def twitch_game_top_streamers(game, auth_info):
+    raise NotImplementedError
 
 # --------------- Functions that modify behavior --------------------------------
 
-def get_welcome_response():
+def get_welcome_response(session):
+    #TODO tell user if they are authenticated
     session_attributes = {}
     card_title = "Welcome to the My Twitch Streams Alexa Skill"
     speech_output = "Welcome to the My Twitch streams app. To use please to do"
     reprompt_text = "To use this skill, please todo"
     should_end_session = False
-    return build_response(session_attributes, build_speechlet_response(card_title,speech_output,reprompt_text, should_end_session))
+
+    return build_response(session_attributes,
+                          build_speechlet_response(card_title,speech_output,reprompt_text, should_end_session))
+
+def get_top_streamers(session):
+    #TODO
+
+    auth_info = get_twitch_auth(session)
+    top_streamers = twitch_top_streamers(5, auth_info)
+
+    session_attributes = {}
+    card_title = "Top Twitch Streamers On Now"
+    speech_output = "Here are the top 5 streamers: " + top_streamers
+    reprompt_text = None
+    should_end_session = False
+
+    return build_response(session_attributes,
+                          build_speechlet_response(card_title, speech_output, reprompt_text, should_end_session))
 
 
+def get_my_top_streamers(session):
+    raise NotImplementedError
+
+def get_game_top_streamers(request, session):
+    raise NotImplementedError
 
 # ---------------------------- Events -------------------------------------------
 
@@ -48,6 +85,15 @@ def on_intent(request, session):
     intent_name = request['intent']['name']
     log_action("Intent ({})".format(intent_name), request, session)
 
+    if intent_name == "GetMyStreams":
+        return get_my_top_streamers(session)
+    elif intent_name == "GetTopStreams":
+        return get_top_streamers(session)
+    elif intent_name == "GetTopStreamsByGame":
+        return get_game_top_streamers(request, session)
+    else:
+        print ("Invalid intent passed in: {}".format(intent_name))
+        raise ValueError("Invalid intent: " + intent_name)
     #TODO handle intents
 
 
