ac13ef4566b11fe44372139df995ec09c6598e69
Jonathon Powell <jonathonwpowell@gmail.com>
2017-04-02 13:42:29 -0400

    Fixed previous commit of wrong file + bug fixes on TTS conversion for streamer names

--- awsLambda/lambda_code.py
+++ awsLambda/lambda_code.py
@@ -1,5 +1,6 @@
 import requests
 
+
 # --------------- Helpers that build all of the responses ----------------------
 # These helpers are from amazon
 
@@ -8,7 +9,7 @@ def build_speechlet_response(title, output, reprompt_text, should_end_session):
     return {
         'outputSpeech': {
             'type': 'PlainText',
-            'text': output
+            'text': switch_names_for_TTS(output)
         },
         'card': {
             'type': 'Simple',
@@ -23,6 +24,7 @@ def build_speechlet_response(title, output, reprompt_text, should_end_session):
         },
         'shouldEndSession': should_end_session
     }
+
 
 def build_login_card_response(title, output, reprompt_text, should_end_session):
     return {
@@ -84,7 +86,7 @@ def twitch_my_top_streamers(num_of_responses, auth_info):
     url = api_info['base_api_url'] + api_info['my_top_streams']
     headers = {"Accept": "application/json",
                "Client-ID": api_info['Client-ID'],
-               "Authorization":"OAuth " + auth_info}
+               "Authorization": "OAuth " + auth_info}
 
     api_response = requests.get(url, headers=headers)
     json = api_response.json()
@@ -146,7 +148,7 @@ def get_welcome_response(session):
     speech_output = "Welcome to the My Twitch streams skill. " \
                     "To get your followed live streams say ask {0} who is streaming. " \
                     "To get the top streams right now say ask {0} what are the top streams. " \
-                    "To get the top streams by game say ask {0} who is streaming League of Legends"\
+                    "To get the top streams by game say ask {0} who is streaming League of Legends" \
         .format(get_skill_invocation_name())
     reprompt_text = None
     should_end_session = True
@@ -189,7 +191,6 @@ def get_my_top_streamers(session):
 
     my_top_streamers = twitch_my_top_streamers(num_of_streams, auth_info)
 
-
     if my_top_streamers is None:
         return get_login_card()
 
@@ -210,8 +211,8 @@ def get_game_top_streamers(request, session):
     if game is None:
         return get_invalid_game_response()
 
-
     streamers = twitch_game_top_streamers(num_of_streams, game)
+
     session_attributes = {}
     card_title = "Top Twitch Streamers On Now For {}".format(game)
     speech_output = "Here are the top available streamers for {}: {}".format(game, streamers)
@@ -232,13 +233,14 @@ def get_invalid_game_response():
     return build_response(session_attributes,
                           build_speechlet_response(card_title, speech_output, reprompt_text, should_end_session))
 
+
 def get_login_card():
     session_attributes = {}
     card_title = "Register With Twitch"
     speech_output = "Thank you for using my twitch streams. " \
                     "Please register with your twitch account if you wish to get information about your followed streams. " \
                     "You can still get the top streams by saying ask {0} what are the top streams, " \
-                    "or get the top streams by game by saying ask {0} who is streaming League of Legends"\
+                    "or get the top streams by game by saying ask {0} who is streaming League of Legends" \
         .format(get_skill_invocation_name())
     reprompt_text = None
     should_end_session = True
@@ -249,6 +251,8 @@ def get_login_card():
 
 def get_skill_invocation_name():
     return "my twitch streams"
+
+
 # ---------------------------- Events -------------------------------------------
 
 def on_intent(request, session):
@@ -318,3 +322,24 @@ def get_twitch_game_name(game):
         return name_dict[game.lower().strip()]
     else:
         return None
+
+
+# ------------------------------------------------------------------------
+
+# This is to change the name of the streamer to a way that Alexa TTS will say it correctly
+def switch_names_for_TTS(toConvert):
+    if toConvert is None:
+        return None
+
+    return_text = toConvert
+    conversion_dict = {}
+
+    with open("channelNameTTSConversion.txt") as f:
+        for line in f:
+            (key, val) = line.split(",")
+            conversion_dict[key.lower().strip()] = val.strip()
+
+    for display_name in conversion_dict:
+        return_text = return_text.replace(display_name, conversion_dict[display_name])
+
+    return return_text
